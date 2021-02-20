ae020b35027ac19ecbdfa7179e98e0a53aa9af13
leo.liang <leo.liang@vendhq.com>
2021-02-21 11:29:12 +1300

    add a workaround

--- executeCypressTest.sh
+++ executeCypressTest.sh
@@ -3,5 +3,9 @@ node --version
 
 cd learnCypress2
 npm install
+# workaround for this issue https://github.com/cypress-io/cypress/issues/4624
+npx cypress cache clear
+npx cypress install
+# end of workaround
 npx cypress run --spec "cypress/integration/examples/actions.spec.js"
 cd -
