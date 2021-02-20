84c8c24edd81f8cd9db5769b5445657d9f1fa997
leo.liang <leo.liang@vendhq.com>
2021-02-21 11:23:38 +1300

    add git clean

--- getCypressCode.sh
+++ getCypressCode.sh
@@ -5,5 +5,5 @@ if [ ! -d "./learnCypress2" ]; then
   git clone --depth=1 https://github.com/nzleoliang/learnCypress2.git
 else
   echo "nobob directory exists, just pulling the latest code"
-  cd learnCypress2 && git pull && cd -
+  cd learnCypress2 && git clean -fdx && git pull && cd -
 fi
