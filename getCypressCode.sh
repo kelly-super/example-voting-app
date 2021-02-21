2139f630244a0aa2177a5e71311baa419e838aba
leo.liang <leo.liang@vendhq.com>
2021-02-21 15:56:48 +1300

    debug

--- getCypressCode.sh
+++ getCypressCode.sh
@@ -2,7 +2,7 @@ set -ex
 
 if [ ! -d "./voting_app_cypress" ]; then
   echo "cloning cypress code..."
-  git clone --depth=1 https://github.com/nzleoliang/voting_app_cypress.git
+  git clone https://github.com/nzleoliang/voting_app_cypress.git
 else
   echo "cypress directory exists, just pulling the latest code"
   cd voting_app_cypress && git pull && cd -
