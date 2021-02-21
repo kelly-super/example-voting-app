0d9d2d971b16ba39e31946f958945afe91e25856
leo.liang <leo.liang@vendhq.com>
2021-02-21 17:38:17 +1300

    tidy up

--- executeCypressTest.sh
+++ executeCypressTest.sh
@@ -1,11 +1,7 @@
 set -ex
 
-# docker-compose -f docker-compose.cypress.yml up --exit-code-from cypress
-# docker-compose -f docker-compose.cypress.yml down
-
 cd voting_app_cypress
 
-# docker run -it -v $PWD:/e2e -w /e2e cypress/included:5.0.0
 npm install
 npx cypress run
 
--- getCypressCode.sh
+++ getCypressCode.sh
@@ -6,6 +6,8 @@ if [ ! -d "./voting_app_cypress" ]; then
 else
   echo "cypress directory exists, just pulling the latest code"
   cd voting_app_cypress && git pull && cd -
+
+  # I think we can comment out below two lines
   touch cypress.json
   echo {} > cypress.json
 fi
