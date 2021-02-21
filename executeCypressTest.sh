a462b0f6b3429818972f3587675fd4a452e155b5
leo.liang <leo.liang@vendhq.com>
2021-02-21 13:21:24 +1300

    user cypress docker image

--- executeCypressTest.sh
+++ executeCypressTest.sh
@@ -1,11 +1,14 @@
-set -ex
+set -x
 node --version
 
-cd learnCypress2
-npm install
+# cd learnCypress2
+# npm install
 # workaround for this issue https://github.com/cypress-io/cypress/issues/4624
-npx cypress cache clear
-npm install --save-dev cypress
+# npx cypress cache clear
+# npm install --save-dev cypress
 # end of workaround
-node_modules/.bin/cypress run --spec "cypress/integration/examples/actions.spec.js"
-cd -
+# node_modules/.bin/cypress run --spec "cypress/integration/examples/actions.spec.js"
+
+docker-compose -f docker-compose.cypress.yml up --exit-code-from cypress
+docker-compose -f docker-compose.cypress.yml down
+# cd -
