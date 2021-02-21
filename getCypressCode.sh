e12adc8cbe32d245b130211524f23dd33ec47259
leo.liang <leo.liang@vendhq.com>
2021-02-21 13:32:17 +1300

    use a new cypress repo

--- getCypressCode.sh
+++ getCypressCode.sh
@@ -2,8 +2,8 @@ set -ex
 
 if [ ! -d "./learnCypress2" ]; then
   echo "cloning cypress code..."
-  git clone --depth=1 https://github.com/nzleoliang/learnCypress2.git
+  git clone --depth=1 https://github.com/nzleoliang/voting_app_cypress.git
 else
   echo "nobob directory exists, just pulling the latest code"
-  cd learnCypress2 && git pull && cd -
+  cd voting_app_cypress && git pull && cd -
 fi
