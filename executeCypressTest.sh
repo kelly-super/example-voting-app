0ea14a98d69d3f8ffc1274dc217c1d13ef659bd3
leo.liang <leo.liang@vendhq.com>
2021-02-21 16:45:22 +1300

    debug

--- executeCypressTest.sh
+++ executeCypressTest.sh
@@ -1,4 +1,10 @@
 set -ex
 
-docker-compose -f docker-compose.cypress.yml up --exit-code-from cypress
-docker-compose -f docker-compose.cypress.yml down
+# docker-compose -f docker-compose.cypress.yml up --exit-code-from cypress
+# docker-compose -f docker-compose.cypress.yml down
+
+cd voting_app_cypress
+
+docker run -it -v $PWD:/e2e -w /e2e cypress/included:5.0.0
+ 
+cd -
